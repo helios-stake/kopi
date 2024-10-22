841573842319c0e1d0a38f06952443cb2de64ccc
dev@kopi.money <dev@kopi.money>
2024-10-22 15:57:51 +0400

    v0.6.2

--- app/upgrades/upgrades.go
+++ app/upgrades/upgrades.go
@@ -1,12 +1,19 @@
 package upgrades
 
-import "github.com/kopi-money/kopi/app/upgrades/v0_6_1"
+import (
+	"github.com/kopi-money/kopi/app/upgrades/v0_6_1"
+	"github.com/kopi-money/kopi/app/upgrades/v0_6_2"
+)
 
 func UpgradeHandlers() Upgrades {
 	return Upgrades{
 		{
 			UpgradeName:          "v0_6_1",
 			CreateUpgradeHandler: v0_6_1.CreateUpgradeHandler,
+		},
+		{
+			UpgradeName:          "v0_6_2",
+			CreateUpgradeHandler: v0_6_2.CreateUpgradeHandler,
 		},
 	}
 }
