7bf71a010a225d8aa4ad508a615f041bc7d1686e
GnaD13 <cpt.meoz@gmail.com>
2024-12-07 23:33:45 +0700

    add capability module version

--- app/upgrades/v0_6_6/upgrade.go
+++ app/upgrades/v0_6_6/upgrade.go
@@ -8,15 +8,12 @@ import (
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/ibc-go/modules/capability"
+	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
 )
 
 func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, k wasmkeeper.Keeper) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		vm, err := mm.RunMigrations(ctx, configurator, vm)
-		if err != nil {
-			return vm, err
-		}
-
 		// Set CosmWasm params
 		wasmParams := wasmtypes.DefaultParams()
 		wasmParams.CodeUploadAccess = wasmtypes.AllowNobody
@@ -25,6 +22,8 @@ func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, 
 			return vm, fmt.Errorf("unable to set CosmWasm params")
 		}
 
-		return vm, nil
+		vm[capabilitytypes.ModuleName] = capability.AppModule{}.ConsensusVersion()
+
+		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
